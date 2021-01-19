e41425c468fbaf4f9c9e63cc5e4ad7d43ffb5ae6
zat-is-me <taahm1@morgan.edu>
2021-01-19 12:37:31 -0600

    initial commit

---
+++ helloVariable.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Tatek";
+var lastName = "Ahmed";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Template output");
+console.log("Hi " + firstName + " " + lastName);
---
+++ helloVariable.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Tatek";
+let lastName: string = "Ahmed";
+
+console.log(found);
+console.log("The grade is "+ grade);
+console.log("Hi "+firstName+ " "+lastName);
+
+console.log("Template output");
+console.log(`Hi ${firstName} ${lastName}`);
